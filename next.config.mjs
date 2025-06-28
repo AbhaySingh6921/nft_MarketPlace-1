e847366f3571623ed185e3cb9cd14dd32b07cb10
root <root@ABHAY-SINGH.localdomain>
2025-06-28 17:24:32 +0000

    Fix: Added .mjs handling in next.config.js for Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -12,11 +12,17 @@ const nextConfig = {
     ],
   },
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      type: "javascript/auto",
+    });
+
     config.resolve.fallback = {
       fs: false,
       net: false,
       tls: false,
     };
+
     return config;
   },
 };
